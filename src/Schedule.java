c0d6a11b5f346aa20130dd3945224d79a7b3b150
lYesterdaYl <bgmds@126.com>
2018-10-27 20:31:30 -0700

    feat
    added needed variable.

--- src/Schedule.java
+++ src/Schedule.java
@@ -8,9 +8,41 @@ import java.util.Scanner;
 
 public class Schedule {
 
+    public static final int N = 5;  // queue size for MLF
+    public static final int T = 1;  // time piece size
+
+    // record the information of the process
+    class Process{
+        int id;
+        int arrival;
+        int service;
+
+        int remain;    // used by srt
+
+        int qtime;     // spent in current queue, used by mlf
+
+        int turnaround;  // final result
+    }
+
+    class MLFQueue{
+        Queue<Process> queue;
+        int piece;   // time piece for this queue
+    }
+
+    private PrintWriter outfile;
+
+    private ArrayList<Process> orgList;
+    private ArrayList<Process> sortedList;
+
+    // default constructor
+    public Schedule(){
+        orgList = new ArrayList<Process>();
+        sortedList  = new ArrayList<Process>();
+    }
+
     // run the 4 test
     public void runTest(String infname, String outfname) throws IOException{
-        
+
     }
     // The main method, start the project
     public static void main(String[] args) throws IOException{
